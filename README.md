8396a980cc30e30ee47706dd67fcdf7f933bf525
SalikSayyed <51844798+SalikSayyed@users.noreply.github.com>
2020-06-02 01:56:22 +0530

    task3 final

---
+++ README.md
@@ -0,0 +1,2 @@
+# MLOPS_task_3
+xslkdnvajng;aksjdgn;skjdgna;skfjnga;kfsngae;fdgbva;eksgnvaksbfgvadfkvbdf
---
+++ model_layers2.py
@@ -0,0 +1,18 @@
+from numpy import mean
+from numpy import std
+from sklearn.model_selection import KFold
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+import model_layers
+from model_layers import *
+model.add(Flatten()) 
+model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(10, activation='softmax'))
+opt = SGD(lr=0.01, momentum=0.9)
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
